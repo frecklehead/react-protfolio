86d9e4b41aa6feea5f9dcfc1dc253d8883471208
frecklehead <prashamsa35np@gmail.com>
2024-05-03 10:03:14 +0545

    1

--- react-protfolio/src/components/navbar/profile/profile.jsx
+++ react-protfolio/src/components/navbar/profile/profile.jsx
@@ -4,10 +4,10 @@ import styles from "./profile.module.css"
 export const Profile=()=>{
   return <section className={styles.container}>
     <div className={styles.content}> 
-      <h1 className={styles.title}>Hi! I'm Prashamsa Aryal.</h1><p className={styles.description}>I am a 2nd year Electronics and Communication Engineering Student currently studying in Pulchowk Campus.
+      <h1 className={styles.title}>Hi! I'm Prashamsa Aryal.</h1><p className={styles.description}>
         
 <br />
-        With a strong foundation in Python and JavaScript, along with experience in frameworks and libraries like NumPy, pandas, scikit-learn, Django, and React, I'm learning to tackle complex challenges in the field of artificial intelligence and software development.
+        I am learner with  strong foundation in Python and JavaScript, along with experience in frameworks and libraries like NumPy, pandas, scikit-learn, Django, and React learning to tackle complex challenges in the field of artificial intelligence and software development.
         </p>
         <a href="mailto:prashamsa35np@gmail.com" className={styles.contact}>Contact Me</a>
         </div>
