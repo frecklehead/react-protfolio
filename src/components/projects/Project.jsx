67015abc02fc047bc1f6311c15aa82c3c84fe989
frecklehead <prashamsa35np@gmail.com>
2024-05-25 14:56:55 +0545

    1

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import styles from './Project.module.css';
 
-const Project = ({ title, description ,link}) => {
+const Project = ({ title, description ,link,Source}) => {
   return (
     <div className={styles.container}>
       <div className={styles.project}>
       <h2 className={styles.projectTitle}>{title}</h2>
       <p className={styles.projectDescription}>{description}</p>
       <p className={styles.projectlink}><a href={link}> link </a></p>
+      <p className={styles.projectSource}> <a href={Source}> Source:</a></p>
 
     </div>
     </div>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,7 +5,8 @@ import styles from './Projects.module.css'; // Assuming you still want to use so
 
 const projectsData = [
   { title: 'Hamro Dokaan', description:"An django based web application for small business owner to have their own platform of e-commerce with direct customer communication via direct messaging."
-,link:"abc.com" },
+,link:"https://youtu.be/VtPlaFJdzNE" 
+,Source:"https://github.com/frecklehead/Online-Marketplace"},
   { title: 'Ecllipses', description: 'An threejs based web app with visually appealing animations of ecllipes especially designed for kids where they can know anyhting about ecllipses' ,link:"abc.com"},
   { title: 'MedicalBillEstimater', description: 'Using Linear regression model with ML model helps us to predict the medical charges of an individual based on his past history.',
 link:"abc.com" }
@@ -20,7 +21,7 @@ const Projects = () => {
        
       {projectsData.map((project, index) => (
         <Project key={index} title={project.title} description={project.description} 
-        link={project.link}/>
+        link={project.link} Soucrce={project.Source}/>
       ))}
     </div>
     
