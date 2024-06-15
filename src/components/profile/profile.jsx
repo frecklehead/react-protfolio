01589cd17dc12ed7fde7e25894ac714d73251dc1
frecklehead <prashamsa35np@gmail.com>
2024-06-15 19:58:04 +0545

    updates

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -4,11 +4,14 @@ import styles from "./profile.module.css"
 export const Profile=()=>{
   return <section  id ="about" className={styles.container}>
     <div className={styles.content}> 
-      <h1 className={styles.title}>Hi! I'm Prashamsa Aryal.</h1><p className={styles.description}>
+      <h1 className={styles.title}>Hi! I'm Prashamsa Aryal.</h1>
+      <div className="about">
+      <p className={styles.description}>
         
-<br />
-        I am learner with  strong foundation in Python and JavaScript, along with experience in frameworks and libraries like NumPy, pandas, scikit-learn, Django, and React learning to tackle complex challenges in the field of artificial intelligence and software development.
-        </p>
+        <br />
+                I am learner with  strong foundation in Python and JavaScript, along with experience in frameworks and libraries like NumPy, pandas, scikit-learn, Django, and React learning to tackle complex challenges in the field of artificial intelligence and software development.
+                </p>
+      </div>
         <a href="mailto:prashamsa35np@gmail.com" className={styles.contact}>Contact Me</a>
         </div>
     
