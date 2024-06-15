d8cab57684bb82efdf1658b55551649ddea75530
frecklehead <prashamsa35np@gmail.com>
2024-06-15 19:34:27 +0545

    digit-recongnizer

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,6 +4,10 @@ import Project from './Project';
 import styles from './Projects.module.css'; // Assuming you still want to use some container styles
 
 const projectsData = [
+  { title: 'Digit-Recognizer', description:"This Django project provides a simple web application that allows users to draw digits on a canvas and receive a prediction of the digit. The prediction is powered by a machine learning model trained on the MNIST dataset."
+,link:"hhttps://x.com/aryal_prashamsa/status/1801974728418050056" 
+,Source:"https://github.com/frecklehead/Digit-Predictor-MNIST-"},
+
   { title: 'Hamro Dokaan', description:"An django based web application for small business owner to have their own platform of e-commerce with direct customer communication via direct messaging."
 ,link:"https://youtu.be/VtPlaFJdzNE" 
 ,Source:"https://github.com/frecklehead/Online-Marketplace"},
